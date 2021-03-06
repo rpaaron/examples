52c373a60a0f3a0490d816e4a3577302e591b417
Aaron Lipinski <aaron.lipinski@roboticsplus.co.nz>
2021-03-06 21:35:24 +1300

    behold!

--- rclcpp/services/minimal_service/main.cpp
+++ rclcpp/services/minimal_service/main.cpp
@@ -20,6 +20,28 @@
 
 using AddTwoInts = example_interfaces::srv::AddTwoInts;
 
+template<typename ServiceT>
+class AsyncService : public rclcpp::Service<ServiceT>
+{
+  rclcpp::AnyServiceCallback<ServiceT> any_callback_;
+  //using rclcpp::Service<ServiceT>::Service;
+
+  void handle_request(std::shared_ptr<rmw_request_id_t> request_header,
+                      std::shared_ptr<void> request) override
+  {
+    auto typed_request = std::static_pointer_cast<typename ServiceT::Request>(request);
+    auto response = std::make_shared<typename ServiceT::Response>();
+    any_callback_.dispatch(request_header, typed_request, response);
+    this->send_response(*request_header, *response);
+  }
+public:
+  AsyncService(std::shared_ptr<rcl_node_t> node_handle, const std::string & service_name, 
+	       rclcpp::AnyServiceCallback<ServiceT> any_callback, rcl_service_options_t& service_options)
+      : rclcpp::Service<ServiceT>::Service(node_handle, service_name, any_callback, service_options),
+	any_callback_(any_callback)
+  {}
+};
+
 class Node : public rclcpp::Node
 {
   using rclcpp::Node::Node;
@@ -29,7 +51,25 @@ public:
   typename rclcpp::Service<ServiceT>::SharedPtr create_async_service(const std::string& service_name,
                                                       CallbackT&& callback)
   {
-    return rclcpp::Node::create_service<ServiceT, CallbackT>(service_name, callback); 
+    auto qos_profile = rmw_qos_profile_services_default;
+    auto group = nullptr;
+    auto node_base = get_node_base_interface();
+    auto node_services = get_node_services_interface();
+
+    rclcpp::AnyServiceCallback<ServiceT> any_service_callback;
+    any_service_callback.set(callback);
+    rcl_service_options_t service_options = rcl_service_get_default_options();
+    service_options.qos = qos_profile;
+
+    auto serv = std::make_shared<AsyncService<ServiceT>>(
+        node_base->get_shared_rcl_node_handle(),
+	service_name,
+	any_service_callback,
+	service_options);
+
+    auto serv_base_ptr = std::dynamic_pointer_cast<rclcpp::ServiceBase>(serv);
+    node_services->add_service(serv_base_ptr, group);
+    return serv;
   }
 };
 
